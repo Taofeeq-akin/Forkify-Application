e6cad6aff06ad0c622e10c7433a037a87c34ad03
Taofeeq akin <taofeeqomoakin18@gmail.com>
2022-10-06 14:02:08 +0100

    Add timeout to race with getJSON using promise.race

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,4 @@
 // In this file we will be keeping reuseable variables and also responsible for kind of defining some important data about the app itself
 
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 10;
--- src/js/helper.js
+++ src/js/helper.js
@@ -1,15 +1,24 @@
 // Will add functions that are reuseble
 
 import { async } from 'regenerator-runtime';
+import { TIMEOUT_SEC } from './config';
+
+const timeout = function (s) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`Request took too long! Timeout after ${s} second`));
+    }, s * 1000);
+  });
+};
 
 export const getJSON = async function (url) {
   try {
-    const res = await fetch(url);
+    const res = await Promise.race([fetch(url), timeout(TIMEOUT_SEC)]);
     const data = await res.json(); // to get data from fetch api and store it to the variable
     // console.log(res, data);
 
     if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-    return data;
+    return data; // will be the resolved value that getJSON function returns
   } catch (err) {
     throw err;
   }
